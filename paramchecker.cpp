dddcf7b5b6ecc48e4eb952cb93b40a6d1ded7e45
Yeshwanth <yeshwanth.sampangi@in.bosch.com>
2020-07-27 17:14:45 +0530

    use multiple apis

--- paramchecker.cpp
+++ paramchecker.cpp
@@ -1,14 +1,32 @@
 
 bool vitalsAreOk(float bpm, float spo2, float respRate) {
-  bool return_val = true;
+  bool bpm_ok, spo2_ok, respRate_ok;
+  bpm_ok = bpmOk(bpm);
+
+  spo2_ok = spo2Ok(spo2);
+
+  respRate_ok = respRateOk(respRate);
+
+  return bpm_ok && spo2_ok && respRate_ok;
+}
+
+bool bpmOk(float bpm) {
   if(bpm < 70 || bpm > 150) {
     return false;
-  } 
-  if(spo2 < 80) {
-    return false;
-  } 
-  if(respRate < 30 || respRate > 60) {
+  }
+  return true;
+}
+
+bool spo2Ok(float spo2) {
+  if(spo2 < 70 || spo2 > 150) {
     return false;
   }
-  return return_val;
+  return true;
+}
+
+bool respRateOk(float respRate) {
+  if(respRate < 70 || respRate > 150) {
+    return false;
+  }
+  return true;
 }
