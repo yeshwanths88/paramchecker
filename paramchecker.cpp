f3c04626d359459f88af225099f0cd965d788994
Yeshwanth <yeshwanth.sampangi@in.bosch.com>
2020-07-27 17:05:56 +0530

    remove nested if else ladder

--- paramchecker.cpp
+++ paramchecker.cpp
@@ -2,11 +2,13 @@
 bool vitalsAreOk(float bpm, float spo2, float respRate) {
   bool return_val = true;
   if(bpm < 70 || bpm > 150) {
-    return_val = false;
-  } else if(spo2 < 80) {
-    return_val = false;
-  } else if(respRate < 30 || respRate > 60) {
-    return_val = false;
+    return false;
+  } 
+  if(spo2 < 80) {
+    return false;
+  } 
+  if(respRate < 30 || respRate > 60) {
+    return false;
   }
   return return_val;
 }
